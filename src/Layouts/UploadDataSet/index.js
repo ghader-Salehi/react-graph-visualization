5749f0914f376351f27c61fcfff8cfa173a62b1d
Ghader Sahlehi <amirghsl1380@gmail.com>
2021-06-06 12:20:40 +0430

    upload dataset page completed

--- src/Layouts/UploadDataSet/index.js
+++ src/Layouts/UploadDataSet/index.js
@@ -1,15 +1,65 @@
-import { Checkbox } from "@material-ui/core";
-import React from "react";
+import { Button, Checkbox, makeStyles ,  } from "@material-ui/core";
+import React, { useState, useRef} from "react";
+import clsx from "clsx";
+import Battery from "../../assets/icons/icons8-battery-96.png";
+import Switch from "../../assets/icons/icons8-light-switch-128.png";
+import Dst from "../../assets/icons/icons8-junction-80.png";
+import data from "../../clean-response.json";
+import Circle from '../../assets/icons/icons8-red-circle-96.png'
+
+const useStyles = makeStyles((theme) => ({
+  fontStyle: {
+    fontFamily: "iranyekan",
+  },
+  batteryPosition: {},
+  switchPosition: {},
+  absloutePosition: {
+    position: "absolute",
+  },
+}));
 
 const Index = () => {
   const [checked, setChecked] = React.useState(false);
 
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  const [showProccessedImage, setShowProccessedImage] = useState(false);
+  const classes = useStyles();
+
   const handleChange = (event) => {
     setChecked(event.target.checked);
   };
-  const handleAddFile = (e)=>{
-    console.log(e.target.files[0]);
-  }
+  const handleAddFile = (e) => {
+    console.log(URL.createObjectURL(e.target.files[0]));
+    setUploadedImageUrl(URL.createObjectURL(e.target.files[0]));
+  };
+  const handleProccess = () => {
+    setShowProccessedImage(true);
+  };
+
+  const setPositionInPage = (top, left) => {
+    return {
+      position: "absolute",
+      top: `${top}%`,
+      left: `${left}%`,
+    };
+  };
+
+  React.useEffect(() => {
+    // const canvasRef = useRef(null)
+
+  
+ 
+   
+  // context.drawImage(<img src={uploadedImageUrl} />, 0, 0);
+   
+
+  }, []);
+
+  const declareType = (type) => {
+    if (type === "distribution") return Dst;
+    else if (type === "source") return Battery;
+    else if (type === "switch") return Switch;
+  };
   return (
     <>
       <div className="d-flex">
@@ -17,19 +67,91 @@ const Index = () => {
           <input onChange={handleAddFile} type="file" />
         </div>
         <div className="d-flex">
-            <div className='m-3 mt-4'>
-                <span>
-                    پردازش بدون جعبه تقیسم در دو طرف در 
-                </span>
-            </div>
+          <div className="m-3 mt-4">
+            <span>پردازش بدون جعبه تقیسم در دو طرف در</span>
+          </div>
           <Checkbox
-            className='p-3'
+            className="p-3"
             checked={checked}
             onChange={handleChange}
             color="primary"
-            inputProps={{ 'aria-label': 'secondary checkbox' }}
+            inputProps={{ "aria-label": "secondary checkbox" }}
           />
         </div>
+      </div>
+      <div className="d-flex justify-content-center">
+        {/* <canvas/> */}
+        {showProccessedImage && (
+          <div style={{ position: "relative" }}>
+            {uploadedImageUrl && (
+              <div>
+                <img
+                  style={{ width: "400px", height: "400px"  }}
+                  src={uploadedImageUrl}
+                ></img>
+              
+
+
+               
+                
+                {data.vertices.map((item, index) => {
+                  return (
+                    <>
+                      <img
+                        key={index}
+                        src={declareType(item.type)}
+                        style={{
+                          position: "absolute",
+                          top: `${item.top}%`,
+                          left: `${item.left}%`,
+                          width: "30px",
+                          height: "30px",
+                        }}
+                      ></img>
+                      
+                    </>
+                  );
+                })}
+                  {data.paths.map((item)=>(
+                   item.paths.map((i)=>{
+                     return(
+                      <img
+                      src={Circle}
+                      style={{
+                        position: "absolute",
+                        top: `${i.top}%`,
+                        left: `${i.left}%`,
+                        width: "20px",
+                        height: "20px",
+                      }}
+                    ></img>
+                     )
+                   })
+                  ))}
+              </div>
+            )}
+          </div>
+        )}
+        <div>
+          {uploadedImageUrl && (
+            <img
+              style={{ width: "400px", height: "400px", marginRight: "5rem" }}
+              src={uploadedImageUrl}
+            ></img>
+          )}
+        </div>
+      </div>
+      <div className="d-flex justify-content-center m-5">
+        {uploadedImageUrl && (
+          <Button
+            onClick={handleProccess}
+            variant="contained"
+            color="primary"
+            className={clsx([classes.fontStyle, ""])}
+          >
+            پردازش
+          </Button>
+        )}
       </div>
     </>
   );
---
+++ src/api/graph.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+
+export const createGraph = async ()=>{
+    return await axios.post();
+}
+
+export const uploadData = async ()=>{
+    return await axios.post();
+}
